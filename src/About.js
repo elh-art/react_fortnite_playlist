2aeb0b4d8a28b76fe8278dd7913322704476c5a0
Balazs Elhart <code.enfaced@aleeas.com>
2022-08-05 15:11:58 +0200

    About and Home page
    Html & Css

--- src/About.js
+++ src/About.js
@@ -2,8 +2,10 @@ import './App.css'
 
 function About() {
   return (
-    <div>
-      <h1>About</h1>
+    <div className='about-container'>
+      <h1 className='aboutSite'>This site is a small single-page <span>React Router</span> v6 application.
+I wrote it to practice this React library. The application fetches data from <a href="https://fortnite-api.com/">Fortnite free API</a> . My root layout is the Navigation bar which is always rendered in the DOM. Otherwise, the app shows the rest of the page based on the URL path changes.</h1>
+      <img src={require('./fortnite_203516.png')} alt="Fortnite characters" />
     </div>
   )
 }
--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,16 @@
+import { Link } from 'react-router-dom'
 import './App.css'
 
 function Home() {
   return (
-    <div>
-      <h1>Home</h1>
+    <div className='about-container'>
+      <h1 className='aboutSite'>
+      Fortnite is a massive online multiplayer game made by Epic Games on Unreal Engine 4. This site presents all the different <Link to="/mainlist">game-modes</Link> available in the game.
+      </h1>
+      <h1 className='aboutSite'>
+      Fortnite has four distinct <Link to="/mainlist">game-modes</Link>: Save the World, Battle Royale, Zero Build, and Creative. You can get detailed information about them under Playlists.  
+      </h1>
+      <img src={require('./Fortnite-Gamemods.jpg')} alt="Fortnite website picture" />
     </div>
   )
 }
