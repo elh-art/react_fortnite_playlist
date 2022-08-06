5f3e84b69a6c7f5048f87c24ef05b3142779a0ae
Balazs Elhart <code.enfaced@aleeas.com>
2022-08-06 15:13:56 +0200

    css responsiveness
    and small fixes in css mainly

--- src/Home.js
+++ src/Home.js
@@ -8,7 +8,7 @@ function Home() {
       Fortnite is a massive online multiplayer game made by Epic Games on Unreal Engine 4. This site presents all the different <Link to="/mainlist">game-modes</Link> available in the game.
       </h1>
       <h1 className='aboutSite'>
-      Fortnite has four distinct <Link to="/mainlist">game-modes</Link>: Save the World, Battle Royale, Zero Build, and Creative. You can get detailed information about them under Playlists.  
+      Fortnite has four distinct <Link to="/mainlist">game-modes</Link>: Save the World, Battle Royale, Zero Build, and Creative. You can get detailed information about them under <Link to="/mainlist">Playlists</Link>.  
       </h1>
       <img src={require('./Fortnite-Gamemods.jpg')} alt="Fortnite website picture" />
     </div>
--- src/PlaylistDetails.js
+++ src/PlaylistDetails.js
@@ -26,16 +26,16 @@ const fetchPlaylist = async() => {
         <img className='detailedPic' src={`https://fortnite-api.com/images/playlists/${id}/showcase.png`} alt={playlist.name} />
         <img className='missionPic' src={`https://fortnite-api.com/images/playlists/${id}/missionicon.png`} alt={playlist.maxTeamSize} />
         <h1 className='listname'>{playlist.name}</h1>
-        <h2 className='subname'>{playlist.subName}</h2>
+        <h3 className='maxteams'>Max number of teams: <span>{playlist.maxTeamSize}</span></h3>
+        <h3 className='maxsquad'>Max squad size: <span>{playlist.maxSquadSize}</span></h3>
         <h3 className='gametype'>Game type: {' '}
               {reactStringReplace(playlist.gameType, 'EFortGameType::', (match, i) => (
               <span> {match} </span>
               ))} 
             </h3>
-        <h3 className='rating'>Rating type: <span>{playlist.ratingType}</span></h3>
         <h3 className='players'>Max Players: <span>{playlist.maxPlayers}</span></h3>
-        <h3 className='maxteams'>Max number of teams: <span>{playlist.maxTeamSize}</span></h3>
-        <h3 className='maxsquad'>Max squad size: <span>{playlist.maxSquadSize}</span></h3>
+        <h3 className='rating'>Rating type: <span>{playlist.ratingType}</span></h3>
+        <h2 className='subname'>{playlist.subName}</h2>
       </div>
       <h3 className='description'>{playlist.description}</h3>
     </div>
