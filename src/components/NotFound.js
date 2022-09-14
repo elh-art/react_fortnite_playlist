50f871b52b09bdffdbd65e8f07600d44e15a799d
elh-art <code.enfaced@aleeas.com>
2022-09-14 21:10:02 +0200

    useNavigate hook for NotFound.js

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,6 +1,16 @@
+import { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
 import '../css/HomeAbout.css'
 
 function NotFound() {
+const navigate = useNavigate()
+
+useEffect(() => {
+  setTimeout(() => {
+    navigate("/")
+  }, 2000)
+}, [])
+
   return (
     <div className='about-container'>
       <h1 className='aboutSite'>
